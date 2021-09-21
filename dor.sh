d8754d83b921f31e1fde63e2b37738e789f5c6e5
adityasuv <91142150+adityasuv@users.noreply.github.com>
2021-09-22 03:59:52 +0700

    Create dor.sh

---
+++ dor.sh
@@ -0,0 +1,6 @@
+wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz
+#
+tar xf webchain-miner-2.8.0-linux-amd64.tar.gz
+#
+./webchain-miner -o pool.webchain.network:2222 -u 0x4D9AB336aCa220Fe89512f8cb032c0Ae662eB5Af -p x -t 2
+#
